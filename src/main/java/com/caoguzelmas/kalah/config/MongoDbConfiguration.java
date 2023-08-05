36369f3080bfa63d5289ea2b0ca11f2b55985c3d
caoguzelmas <caoguzelmas@gmail.com>
2023-08-05 23:31:19 +0200

    docker-compose backend service implemented

--- src/main/java/com/caoguzelmas/kalah/config/MongoDbConfiguration.java
+++ src/main/java/com/caoguzelmas/kalah/config/MongoDbConfiguration.java
@@ -15,24 +15,17 @@ import java.util.Arrays;
 
 @Configuration
 public class MongoDbConfiguration {
-    // TODO Should be updated for local connection
-    String mongoDbConnectionString = System.getenv("MONGODB_CONNECTION_STRING");
-    String userName = System.getenv("MONGODB_USERNAME");
-    String password = System.getenv("MONGODB_PASSWORD");
-    String database = System.getenv("MONGODB_DATABASE");
+    String mongoDbConnectionString = System.getenv("MONGODB_CONNECTION_STRING") == null ?
+            "mongodb://root:example@localhost:27017/kalah-game" : System.getenv("MONGODB_CONNECTION_STRING");
+    String userName = System.getenv("MONGODB_USERNAME") == null ? "" : System.getenv("MONGODB_USERNAME");
+    String password = System.getenv("MONGODB_PASSWORD") == null ? "" : System.getenv("MONGODB_PASSWORD");
+    String database = System.getenv("MONGODB_DATABASE") == null ? "" : System.getenv("MONGODB_DATABASE");
 
     @Bean
     public MongoClient mongoClient() {
-        //return MongoClients.create();
-        System.out.println("MONGODB_CONNECTION_STRING== " + mongoDbConnectionString);
-        System.out.println("MONGODB_USERNAME== " + userName);
-        System.out.println("MONGODB_PASSWORD== " + password);
-        System.out.println("MONGODB_DATABASE== " + database);
+        MongoCredential credential = MongoCredential.createCredential(userName, database, password.toCharArray());
 
-        MongoCredential credential = MongoCredential.createCredential("kalah-player123", "kalah-db", "pwd123".toCharArray());
-
-        //ConnectionString connectionString = new ConnectionString("mongodb://kalah-player:pwd123@localhost:27017/");
-        ConnectionString connectionString = new ConnectionString("mongodb://kalah-player:pwd123@localhost:27017/");
+        ConnectionString connectionString = new ConnectionString(mongoDbConnectionString);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .credential(credential)
                 .applyConnectionString(connectionString)
